81eafe82343a20ced4e5eb8bbe0500909bc021dc
LGmedia954 <LeslieGonzalez@me.com>
2022-05-18 21:15:18 -0400

    removed password for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -55,7 +55,7 @@ class Api::V1::UsersController < ApplicationController
     end
       
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :email)
     end
 
  end
