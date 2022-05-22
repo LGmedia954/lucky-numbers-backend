aac47a9417304811331798a6234ac36a352b690b
LGmedia954 <LeslieGonzalez@me.com>
2022-05-22 11:51:11 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :users
+      resources :users do
+        resources :rounds, only: [:new, :create, :index, :show, :destroy]
+      end
+      
       resources :rounds
+      resources :users
 
     end
   end
