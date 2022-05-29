5c1db2d952f67328f00c596e7df8299e50454c28
LGmedia954 <LeslieGonzalez@me.com>
2022-05-29 08:44:17 -0400

    email check prior to create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,14 +14,10 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    emailcheck ||= @user = User.new(user_params)
 
     if @user.save
       render json: UserSerializer.new(@user), status: :created
-    else
-      if User.exists?(params[:id][:email])
-        @user = User.find(params[:id])
-      end
     else
       resp = {
         error: @user.errors.full_messages.to_sentence
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, uniqueness: true
 
+  def emailcheck
+    if User.exists?(params[:email])
+      @user = User.find(params[:email])
+    end
+  end
+
 end
