581d4873c5d7c588870b79f75995bc686828e6eb
LGmedia954 <LeslieGonzalez@me.com>
2022-06-11 11:46:30 -0400

    round serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+#byebug
     if @user.save
       render json: UserSerializer.new(@user), status: :created
     else
--- app/serializers/user_round_serializer.rb
+++ app/serializers/user_round_serializer.rb
@@ -2,8 +2,7 @@ class UserRoundSerializer
   include JSONAPI::Serializer
   attributes :id, :round_id, :user_id
 
-  # # 500 error as we try to add round
-  # # belongs_to :user
-  # # belongs_to :round
+  belongs_to :user
+  belongs_to :round
 
 end
